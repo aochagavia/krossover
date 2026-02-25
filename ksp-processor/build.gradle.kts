0de509197ada5cf2c1ab33d007247175b1f576b5
Adolfo Ochagavía <github@adolfo.ochagavia.nl>
2026-02-25 13:25:57 -0300

    Update to the latest KSP API

--- ksp-processor/build.gradle.kts
+++ ksp-processor/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation("nl.ochagavia.krossover:shared-internals:${project.version}")
-    implementation("com.google.devtools.ksp:symbol-processing-api:1.5.30-1.0.0")
+    implementation("com.google.devtools.ksp:symbol-processing-api:2.3.6")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.9.0")
 }
 
